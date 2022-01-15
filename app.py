69cd36cc6c349713427c2f0eaa11c6a2f8c05b1f
rishi-21111992 <rishi.official.2111@gmail.com>
2022-01-16 00:34:34 +0530

    g

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, url_for, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import pickle
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
 
 # Create the application.
 app = flask.Flask(__name__)
@@ -23,7 +24,26 @@ def login():
         pipeline = pickle.load(open('pickle/user_based_recomm.pkl', 'rb'))
         sr = pipeline.loc[name].sort_values(ascending=False)[0:20] ## series
         top_20_products = pd.DataFrame({'name':sr.index})
-        return  render_template('view.html',tables=[top_20_products.to_html(classes='name')], titles = ['NAN', 'Top 20 Prediction'])
+		
+		reviews = pd.read_csv('/dataset/sample30.csv')
+		reviews['name'].isin(top_20_products['name'])
+		
+		top_20_reviews = reviews[reviews['name'].isin(top_20_products['name'])][['name','reviews_text']]
+		
+		transformer = TfidfTransformer()
+		loaded_vec = CountVectorizer(decode_error="replace",vocabulary=pickle.load(open('/pickle/vector', 'rb')))
+		test_data_features = transformer.fit_transform(loaded_vec.fit_transform(top_20_reviews['reviews_text']))
+		
+		loaded_model = pickle.load(open("/pickle/LRModel", 'rb'))
+		result1 = loaded_model.predict(test_data_features)
+		
+		top_20_reviews['sentiment'] = result1.tolist()
+		
+		top = top_20_reviews.groupby(['name']).sum()
+		
+		top5 = top.sort_values(by='sentiment',ascending=False)[:5]
+
+        return  render_template('view.html',tables=[top5.to_html(classes='name')], titles = ['NAN', 'Top 5 Prediction'])
 	    
 
 if __name__ == '__main__':
