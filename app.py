7136604142196fef5697265dd84ec8422bbc8095
rishi-21111992 <rishi.official.2111@gmail.com>
2022-01-16 01:23:25 +0530

    g

--- app.py
+++ app.py
@@ -41,10 +41,9 @@ def login():
         top = top_20_reviews.groupby(['name']).mean()
         
         top5 = top.sort_values(by='sentiment',ascending=False)[:5]
-        top5.reset_index(level=0, inplace=True)
-        top5 = top5['name']
+        top_5_products = pd.DataFrame({'name':top5.name})
 
-        return  render_template('view.html',tables=[top5.to_html(classes='name')], titles = ['NAN', 'Top 5 Prediction'])
+        return  render_template('view.html',tables=[top_5_products.to_html(classes='name')], titles = ['NAN', 'Top 5 Prediction'])
 
 
 if __name__ == '__main__':
