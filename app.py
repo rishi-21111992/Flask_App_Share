2fa44a793cb88ae777b26d159c59668f88ccdebc
rishi-21111992 <rishi.official.2111@gmail.com>
2022-01-15 20:42:31 +0530

    pushed new file

--- app.py
+++ app.py
@@ -9,16 +9,15 @@ def index():
     return flask.render_template('index.html')
 
 @app.route('/login',methods=['POST'])
-def home():
+def login():
     """Login Form"""
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        name = request.form['username']
-        pipeline = pickle.load(open('pickle/user_based_recomm.pkl', 'rb'))
-        sr = pipeline.loc[name].sort_values(ascending=False)[0:20] ## series
-        top_20_products = pd.DataFrame({'name':sr.index})
-        return  render_template('view.html',tables=[top_20_products.to_html(classes='name')], titles = ['NAN', 'Top 20 Prediction'])
+flask.render_template('login.html')
+    
+name = request.form['username']
+pipeline = pickle.load(open('pickle/user_based_recomm.pkl', 'rb'))
+sr = pipeline.loc[name].sort_values(ascending=False)[0:20] ## series
+top_20_products = pd.DataFrame({'name':sr.index})
+return  render_template('view.html',tables=[top_20_products.to_html(classes='name')], titles = ['NAN', 'Top 20 Prediction'])
 
 
 if __name__ == '__main__':
@@ -26,8 +25,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     
-
-
-
 
 
