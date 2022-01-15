466845720ee539ce93ed9ce2006d1ba0cb452cec
rishi-21111992 <rishi.official.2111@gmail.com>
2022-01-15 20:17:24 +0530

    pushed new file

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
-"""Flask Login Example and instagram fallowing find"""
+import flask
+import os
 
-from flask import Flask, url_for, render_template, request, redirect, session
-from flask_sqlalchemy import SQLAlchemy
-from instagram import getfollowedby, getname
+# Create the application.
+app = flask.Flask(__name__)
 
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-
-
-#loading the sparse file which have processed features
-# Raw File which have missing values , Outlier , ..
-
-# You can python to process and creat final DF or object which then pass in Pickle
-
-# loading the data 
-# here i am loading npz , you can load csv , xlsx , databse conenection 
-#Xtest_Scenerio1_for_flask  = sparse.load_npz("dataset/Xtest_Scenerio1_for_flask.npz")
-# here you can database connector 
-# external API (Twitter API )
-
-#Xtest_Scenerio1_for_flask  - holding my data whch will render on UI 
-
-# http:baseurl/age_prediction
-
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/login',methods=['POST'])
+def home():
     """Login Form"""
     if request.method == 'GET':
         return render_template('login.html')
@@ -39,13 +21,11 @@ def login():
         return  render_template('view.html',tables=[top_20_products.to_html(classes='name')], titles = ['NAN', 'Top 20 Prediction'])
 
 
-
-# Any HTML template in Flask App render_template
-
-if __name__ == '__main__' :
-    app.run(debug=True )  # this command will enable the run of your flask app or api
+if __name__ == '__main__':
+    app.debug=True
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
-    #,host="0.0.0.0")
 
 
 
